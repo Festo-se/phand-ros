5843ab96bd9cc1b85ad5f03198115c5387267faa
tswa <timo.schwarzer@festo.com>
2020-02-05 15:42:12 +0100

    using the new simple pressure functions

--- festo_phand_driver/src/festo_phand_udp_driver.py
+++ festo_phand_driver/src/festo_phand_udp_driver.py
@@ -136,7 +136,7 @@ class ROSPhandUdpDriver():
 
         resp = SetConfigurationResponse()
 
-        grip_config = self.phand.set_grip_config(msg.configuration.grip_configuration)
+        grip_config = self.phand.set_grip_config_pressure(msg.configuration.grip_configuration)
         ctrl_config = self.phand.set_ctrl_mode(msg.configuration.control_configuration)
 
         if grip_config and ctrl_config:
@@ -174,7 +174,7 @@ class ROSPhandUdpDriver():
 
         resp = SimpleOpenResponse()
 
-        if self.phand.simple_open():
+        if self.phand.simple_open_pressure():
 
             rospy.loginfo("Opening the hand.")
             resp.success = True      
